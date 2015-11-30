27a770f78bfe4b816bfb2a58976c0db76923100a
igororeshin <mail.sm_@mail.ru>
2015-12-01 01:31:47 +0300

    Create stringandnumbs1.pas

---
+++ stringandnumbs1.pas
@@ -0,0 +1,16 @@
+function is_digit(c: char): boolean;
+begin
+  is_digit := ('0' <= c) and (c <= '9');
+end;
+
+var
+  c : char;
+  a : boolean;
+begin
+  readln(c);
+  a := is_digit(c);
+  if a = true then
+    write('Символ является цифрой')
+  else
+    write('Символ не является цифрой');
+end.
