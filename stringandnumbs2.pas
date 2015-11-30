51be54e16d85c00e9b6982fcbd55d010c14ef683
igororeshin <mail.sm_@mail.ru>
2015-12-01 01:42:52 +0300

    Update stringandnumbs2.pas

--- stringandnumbs2.pas
+++ stringandnumbs2.pas
@@ -1,6 +1,18 @@
+function is_digit(c: char): boolean;
+begin
+  is_digit := ('0' <= c) and (c <= '9');
+end;
+
 function to_digit(c : char): integer;
 begin
-  to_digit := integer(c) - integer('0');
+  if is_digit(c)d = true then
+    begin
+      to_digit := integer(c) - integer('0');
+    end
+  else
+    begin
+      to_digit := -1;
+    end;
 end;
 
 var 
